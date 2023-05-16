93560c3a57164784c77994688005f6bc4a23a22a
Venukumar34639963 <107405621+vikkile@users.noreply.github.com>
2023-05-16 22:23:30 +0530

    Update car.cpp

--- car.cpp
+++ car.cpp
@@ -1,10 +1,9 @@
 #include "car.hpp"
-#include "iostream"
-using namespace std;
 
 int main() {
     //Car object initialization
     Car FORD;
+    //member variable initialization object initialization
     FORD.ReleaseYear = 2024;
     FORD.customer = "Venu";
     FORD.speed = 0;
@@ -13,9 +12,12 @@ int main() {
     cout << "Car customer name= " << FORD.get_customer() << endl;
     cout << "Car current speed= " << FORD.get_speed() << endl;
     
+    //Accelerating the speed five times
     for(int i=0; i<5 ; i++) {
         FORD.accelerate ();
     }
+    
+    //Deaccelerating the speed five times
     cout << "Car speed after 5 accelerations  = " << FORD.get_speed() << endl;
     for(int i=0; i<5 ; i++) {
         FORD.brake();
